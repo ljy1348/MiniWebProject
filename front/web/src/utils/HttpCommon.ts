6ddc04ab08be62653bb3d6156243feba77b62f23
ljy1348 <141599648+ljy1348@users.noreply.github.com>
2023-11-14 13:58:24 +0900

    Update HttpCommon.ts

--- front/web/src/utils/HttpCommon.ts
+++ front/web/src/utils/HttpCommon.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://ip-172-31-46-247.ap-northeast-2.compute.internal:8080/api",
+  // baseURL: "http://ip-172-31-46-247.ap-northeast-2.compute.internal:8080/api",
+  baseURL: "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json"
   }
