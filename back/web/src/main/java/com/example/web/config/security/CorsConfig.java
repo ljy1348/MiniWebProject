5801bb5275d40b3cbd0bbf87946e875e7c7816ba
ljy1348 <141599648+ljy1348@users.noreply.github.com>
2023-11-14 12:06:49 +0900

    Update CorsConfig.java

--- back/web/src/main/java/com/example/web/config/security/CorsConfig.java
+++ back/web/src/main/java/com/example/web/config/security/CorsConfig.java
@@ -9,20 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-        @Value("http://localhost:3000")
+        @Value("http://localhost")
         private String localport;
 
     @Value("http://${FRONT_HOST}")
     private String frontHost;
 
-        @Value("${front.proxy.http}")
-        private String proxy;
-
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(localport, proxy,frontHost) // React 앱의 주소
+                .allowedOrigins(localport,frontHost) // React 앱의 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*")
                 .exposedHeaders("*");
